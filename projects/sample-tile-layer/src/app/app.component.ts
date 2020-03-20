b8c0a75ec9329abfe0e70460538ccb2d2cce3052
Giorgio <gio.tabarani@gmail.com>
2020-03-20 07:20:25 +0100

    revert file

--- projects/sample-tile-layer/src/app/app.component.ts
+++ projects/sample-tile-layer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,5 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
 }
