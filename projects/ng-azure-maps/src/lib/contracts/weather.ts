ce652d208fd7e807bbf071cc40974b9ce24a6f79
Arnaud Leclerc <A.Leclerc@tekaris.com>
2020-09-09 22:11:06 +0200

    Adding daily, hourly and minute forecast

--- projects/ng-azure-maps/src/lib/contracts/weather.ts
+++ projects/ng-azure-maps/src/lib/contracts/weather.ts
@@ -275,3 +275,488 @@ export interface WeatherUnit {
    */
   unitType: number;
 }
+
+export interface DailyForecastResponse {
+  /**
+   * Summary for the main conditions for the requested time period. Notice that summary can cover only part of the time period.
+   */
+  summary: DailyForecastSummary;
+  /**
+   * Forecast data for each requested day.
+   */
+  forecasts: DailyForecast[];
+}
+
+export interface DailyForecast {
+  /**
+   * Date and time of the current observation displayed in ISO 8601 format, for example, 2019-10-27T19:39:57-08:00.
+   */
+  date: string;
+  /**
+   * Temperature values for the day.
+   */
+  temperature: WeatherUnitRange;
+  /**
+   * RealFeel™ Temperature being returned.
+   */
+  realFeelTemperature: WeatherUnitRange;
+  /**
+   * RealFeel™ Temperature being returned. Describes what the temperature really feels like in the shade.
+   */
+  realFeelTemperatureShade: WeatherUnitRange;
+  /**
+   * Hours of sun.
+   */
+  hoursOfSun: number;
+  /**
+   * Summary of Heating Degree Day or Cooling Degree Day information
+   */
+  degreeDaySummary: DegreeDaySummary;
+  /**
+   * airAndPollen
+   */
+  airAndPollen: AirAndPollen[];
+  /**
+   * Day
+   */
+  day: DayOrNight;
+  /**
+   * Night
+   */
+  night: DayOrNight;
+  /**
+   * Source(s) of the forecast data.
+   */
+  sources: string[];
+}
+
+export interface DayOrNight {
+  /**
+   * Numeric value representing an image that displays the iconPhrase. Please refer to Weather Service Concepts for details.
+   */
+  iconCode: number;
+  /**
+   * Phrase description of the icon. Displayed in specified language. For example, 'Sunny'.
+   */
+  iconPhrase: string;
+  /**
+   * Indicates the presence or absence of precipitation. True indicates the presence of precipitation, false indicates the absence of precipitation.
+   */
+  hasPrecipitation: boolean;
+  /**
+   * Specifies the type of precipitation ("rain" "snow" "ice" or "mix"). If dbz = zero, precipitationType is not present in the response.
+   */
+  precipitationType: string;
+  /**
+   * Description of the intensity.
+   */
+  precipitationIntensity: string;
+  /**
+   * Phrase description of the forecast in specified language. Azure Maps attempts to keep this phrase under 30 characters in length, but some languages/weather events may result in a longer phrase length, exceeding 30 characters.
+   */
+  shortPhrase: string;
+  /**
+   * Phrase description of the forecast in specified language. Azure Maps attempts to keep this phrase under 100 characters in length, but some languages/weather events may result in a longer phrase length, exceeding 100 characters.
+   */
+  longPhrase: string;
+  /**
+   * Percent representing the probability of precipitation. For example, '20'.
+   */
+  precipitationProbability: number;
+  /**
+   * Percent representing the probability of a thunderstorm. For example, '80'.
+   */
+  thunderstormProbability: number;
+  /**
+   * Percent representing the probability of rain. For example, '40'.
+   */
+  rainProbability: number;
+  /**
+   * Percent representing the probability of snow. For example, '30'.
+   */
+  snowProbability: number;
+  /**
+   * Percent representing the probability of ice. For example, '30'.
+   */
+  iceProbability: number;
+  /**
+   * Wind details being returned including speed and direction.
+   */
+  wind: Wind;
+  /**
+   * Wind gust. Wind gust is a sudden, brief increase in speed of the wind.
+   */
+  windGust: Wind;
+  /**
+   * Total liquid equivalent of precipitation during the forecast period.
+   */
+  totalLiquid: WeatherUnit;
+  /**
+   * Rain
+   */
+  rain: WeatherUnit;
+  /**
+   * Snow
+   */
+  snow: WeatherUnit;
+  /**
+   * Ice
+   */
+  ice: WeatherUnit;
+  /**
+   * Hours of precipitation
+   */
+  hoursOfPrecipitation: number;
+  /**
+   * Hours of rain.
+   */
+  hoursOfRain: number;
+  /**
+   * Hours of snow.
+   */
+  hoursOfSnow: number;
+  /**
+   * Hours of ice.
+   */
+  hoursOfIce: number;
+  /**
+   * Percent representing cloud cover.
+   */
+  cloudCover: number;
+  localSource?: LocalSource;
+}
+
+export interface AirAndPollen {
+  /**
+   * Name of the pollen or pollutant. For example, grass, mold, weed, air quality, tree and UV index.
+   */
+  name: string;
+  /**
+   * Value of the given type above. Values associated with mold, grass, weed and tree are in units of parts per cubic meter. Both air quality and UV are indices, so they are unitless.
+   */
+  value: number;
+  /**
+   * Category of the air quality or pollution type. For example, low, high, good, moderate, unhealthy, hazardous.
+   */
+  category: string;
+  /**
+   * Value associated with the air quality or pollution category. These values range from 1 to 6. 1 implying good conditions, 6 implying hazardous conditions.
+   */
+  categoryValue: number;
+  /**
+   * Only exists for air quality. Examples include ozone and particle pollution.
+   */
+  type?: string;
+}
+
+export interface DegreeDaySummary {
+  /**
+   * Number of degrees that the mean temperature is below 65 degrees F/ 18 degree C.
+   */
+  heating: WeatherUnit;
+  /**
+   * Number of degrees that the mean temperature is above 65 degrees F/ 18 degree C.
+   */
+  cooling: WeatherUnit;
+}
+
+/**
+ * Returned temperature values.
+ */
+export interface WeatherUnitRange {
+  /**
+   * Minimum temperature for the time period.
+   */
+  minimum: WeatherUnit;
+  /**
+   * Maximum temperature for the time period
+   */
+  maximum: WeatherUnit;
+}
+
+/**
+ * Summary for the main conditions for the requested time period. Notice that summary can cover only part of the time period.
+ */
+export interface DailyForecastSummary {
+  /**
+   * Date and time that the summary is in effect, displayed in ISO 8601 format, for example, 2019-10-27T19:39:57-08:00.
+   */
+  startDate: string;
+  /**
+   * Date and time that the summary period ends, displayed in ISO 8601 format, for example, 2019-10-27T19:39:57-08:00.
+   */
+  endDate: string;
+  /**
+   * severity
+   */
+  severity: number;
+  /**
+   * Summary phrase of the daily forecast. Displayed in specified language.
+   */
+  phrase: string;
+  /**
+   * one or 2 word(s) to summarize the phrase.
+   */
+  category: string;
+}
+
+export interface LocalSource {
+  /**
+   * Numeric identifier, unique to the local data provider.
+   */
+  id: number;
+  /**
+   * Name of the local data provider. Name is displayed in the language specified by language code in URL, if available. Otherwise, Name is displayed in English or the language in which the name was provided.
+   */
+  name: string;
+  /**
+   * Weather code provided by the local data provider. This weather code allows the forecast to be matched to icons provided by the local data provider instead of Azure Maps icons.
+   */
+  weatherCode: string;
+}
+
+export interface HourlyForecastResponse {
+  forecasts: HourlyForecast[];
+}
+
+export interface HourlyForecast {
+  /**
+   * Date and time of the forecast in ISO 8601 format, for example, 2019-10-27T19:39:57-08:00.
+   */
+  date: string;
+  /**
+   * Numeric value representing an image that displays the iconPhrase.
+   */
+  iconCode: number;
+  /**
+   * Phrase description of the weather icon.
+   */
+  iconPhrase: string;
+  /**
+   * Indicates the presence or absence of precipitation. True indicates the presence of precipitation, false indicates the absence of precipitation.
+   */
+  hasPrecipitation: boolean;
+  /**
+   * Specifies whether or not it is daylight. True indicates day light.
+   */
+  isDaylight: boolean;
+  /**
+   * Temperature being returned.
+   */
+  temperature: WeatherUnit;
+  /**
+   * RealFeel™ Temperature being returned. Describes what the temperature really feels like in the shade.
+   */
+  realFeelTemperature: WeatherUnit;
+  /**
+   * The temperature to which air may be cooled by evaporating water into it at constant pressure until it reaches saturation.
+   */
+  wetBulbTemperature: WeatherUnit;
+  /**
+   * The dewpoint temperature in specified unit. The dewpoint temperature is the temperature that the air must be cooled to in order to reach saturation.
+   */
+  dewPoint: WeatherUnit;
+  /**
+   * Wind details being returned including speed and direction.
+   */
+  wind: Wind;
+  /**
+   * Wind gust. Wind gust is a sudden, brief increase in speed of the wind.
+   */
+  windGust: Wind;
+  /**
+   * Relative humidity is the amount of water vapor present in air expressed as a percentage of the amount needed for saturation at the same temperature.
+   */
+  relativeHumidity: number;
+  /**
+   * Visibility in specified unit. A measure of the distance at which an object or light can be clearly discerned.
+   */
+  visibility: WeatherUnit;
+  /**
+   * Percent representing cloud cover.
+   */
+  cloudCover: number;
+  /**
+   * Cloud ceiling in specified unit. The ceiling is a measurement of the height of the base of the lowest clouds.
+   */
+  ceiling: WeatherUnit;
+  /**
+   * Measure of the strength of the ultraviolet radiation from the sun. Supported values are:
+    0-2 - Low danger from the sun's UV rays or the average person.
+    3-5 - Moderate risk of harm from unprotected sun exposure.
+    6-7 - High risk of harm from unprotected sun exposure.
+    8-10 - Very high risk of harm from unprotected sun exposure.
+    11+ - Extreme risk of harm from unprotected sun exposure.
+   */
+  uvIndex: number;
+  /**
+   * Phrase associated with the uvIndex.
+   */
+  uvIndexPhrase: string;
+  /**
+   * Percent representing the probability of precipitation. For example, '20'.
+   */
+  precipitationProbability: number;
+  /**
+   * Percent representing the probability of rain. For example, '50'.
+   */
+  rainProbability: number;
+  /**
+   * Percent representing the probability of snow. For example, '50'.
+   */
+  snowProbability: number;
+  /**
+   * Percent representing the probability of ice. For example, '5'.
+   */
+  iceProbability: number;
+  /**
+   * Total liquid equivalent of precipitation during the forecast period.
+   */
+  totalLiquid: WeatherUnit;
+  /**
+   * Rain
+   */
+  rain: WeatherUnit;
+  /**
+   * Snow
+   */
+  snow: WeatherUnit;
+  /**
+   * Ice
+   */
+  ice: WeatherUnit;
+}
+
+export interface MinuteForecastResponse {
+  /**
+   * Phrase summaries for the entire forecast period.
+   */
+  summary: MinuteForecastSummary;
+  /**
+   * Summary information for each interval in the forecast. The Summaries breaks down each potential interval where precipitation starts and stops.
+   */
+  intervalSummaries: IntervalSummary[];
+  /**
+   * Forecast data for each interval in the forecast.
+   */
+  intervals: Interval[];
+}
+
+export interface Interval {
+  /**
+   * The date and time for the start of the interval in ISO 8601 format, for example, 2019-10-27T19:39:57-08:00.
+   */
+  startTime: string;
+  /**
+   * The first minute for the interval.
+   */
+  minute: number;
+  /**
+   * A unit that represents forecasted precipitation intensity.
+   */
+  dbz: number;
+  /**
+   * A short phrase describing precipitation condition for the interval.
+   */
+  shortPhrase: string;
+  /**
+   * Key that specifies the threshold value. Along with precipitationType, can be used to determine the simplifiedColor. If dbz is zero, not present in the response.
+   */
+  threshold?: string;
+  /**
+   * The full spectrum color that maps to the dBZ (decibel relative to Z). If dbz is zero, color is not present in the response.
+   */
+  color?: Color;
+  /**
+   * The band color that maps to the precipitation type and threshold. If dbz is zero, not present in the response.
+   */
+  simplifiedColor?: Color;
+  /**
+   * Specifies the type of precipitation ("rain" "snow" "ice" or "mix"). If dbz is zero, precipitationType is not present in the response.
+   */
+  precipitationType?: string;
+  /**
+   * Numeric value representing an image that displays the iconPhrase.
+   */
+  iconCode: number;
+  /**
+   * Percent representing cloud cover.
+   */
+  cloudCover: number;
+}
+
+export interface Color {
+  /**
+   * Red component of the RGB value.
+   */
+  red: number;
+  /**
+   * Green component of the RGB value.
+   */
+  green: number;
+  /**
+   * Blue component of the RGB value
+   */
+  blue: number;
+  /**
+   * Hexadecimal color value.
+   */
+  hex: string;
+}
+
+export interface IntervalSummary {
+  /**
+   * The first minute to which the summary applies.
+   */
+  startMinute: number;
+  /**
+   * The last minute to which the summary applies.
+   */
+  endMinute: number;
+  /**
+   * The number of minutes for which the summary applies.
+   */
+  totalMinutes: number;
+  /**
+   * Short summary phrase. Phrase length is approximately 25 characters.
+   */
+  shortPhrase: string;
+  /**
+   * Brief summary phrase. Phrase length is approximately 60 characters.
+   */
+  briefPhrase: string;
+  /**
+   * Long summary phrase. Phrase length is 60+ characters.
+   */
+  longPhrase: string;
+  /**
+   * Numeric value representing an image that displays the iconPhrase.
+   */
+  iconCode: number;
+}
+
+/**
+ * Phrase summaries for the entire forecast period.
+ */
+export interface MinuteForecastSummary {
+  /**
+   * Summary phrase for the next 60 minutes. Phrase length is approximately 60 characters.
+   */
+  briefPhrase60: string;
+  /**
+   * Short summary phrase for the next 120 minutes. Phrase length is approximately 25 characters.
+   */
+  shortPhrase: string;
+  /**
+   * Summary phrase for the next 120 minutes. Phrase length is approximately 60 characters.
+   */
+  briefPhrase: string;
+  /**
+   * Long summary phrase for the next 120 minutes. Phrase length is 60+ characters.
+   */
+  longPhrase: string;
+  /**
+   * Numeric value representing an image that displays the iconPhrase.
+   */
+  iconCode: number;
+}
--- projects/ng-azure-maps/src/lib/services/weather-service.ts
+++ projects/ng-azure-maps/src/lib/services/weather-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CurrentConditionsResponse, Unit } from '../contracts';
+import { CurrentConditionsResponse, DailyForecastResponse, HourlyForecastResponse, MinuteForecastResponse, Unit } from '../contracts';
 
 @Injectable()
 export class WeatherService {
@@ -33,7 +33,7 @@ export class WeatherService {
   public getCurrentConditions(latitude: number,
     longitude: number,
     details?: boolean,
-    duration?: number,
+    duration?: 0 | 6 | 24,
     language?: string,
     unit?: Unit
   ): Observable<CurrentConditionsResponse> {
@@ -58,6 +58,107 @@ export class WeatherService {
 
     return this.httpClient.get<CurrentConditionsResponse>(url);
 
+  }
+
+  /**
+   * The service returns detailed weather forecast such as temperature and wind by day for the next 1, 5, 10, 15, 25, or 45 days for a given coordinate location. The response include details such as temperature, wind, precipitation, air quality, and UV index.
+   * @param latitude Latitude
+   * @param longitude Longitude
+   * @param duration Specifies for how many days the daily forecast responses are returned. Available values are
+                      1 - Return forecast data for the next day. Returned by default.
+                      5 - Return forecast data for the next 5 days.
+                      10 - Return forecast data for the next 10 days.
+                      25 - Return forecast data for the next 25 days. Only available in S1 SKU.
+                      45 - Return forecast data for the next 45 days. Only available in S1 SKU.
+   * @param language Language in which search results should be returned. Should be one of supported IETF language tags, case insensitive. When data in specified language is not available for a specific field, default language is used. Default value is en-us.
+   * @param unit Specifies to return the data in either metric units or imperial units. Default value is metric.
+   */
+  public getDailyForecast(latitude: number,
+    longitude: number,
+    duration?: 1 | 5 | 10 | 25 | 45,
+    language?: string,
+    unit?: Unit
+  ): Observable<DailyForecastResponse> {
+    let url = `${this._rootUrl}/weather/forecast/daily/json?api-version=${this._apiVersion}&query=${latitude},${longitude}`;
+
+    if (!(duration === null || duration === undefined)) {
+      url += `&duration=${duration}`;
+    }
+
+    if (!(language === null || language === undefined)) {
+      url += `&language=${language}`;
+    }
+
+    if (!(unit === null || unit === undefined)) {
+      url += `&unit=${unit};`
+    }
+
+    return this.httpClient.get<DailyForecastResponse>(url);
+  }
+
+  /**
+   * Request detailed weather forecast by the hour for the next 1, 12, 24 (1 day), 72 (3 days), 120 (5 days), and 240 hours (10 days) for the given the given coordinate location. The API returns details such as temperature, humidity, wind, precipitation, and ultraviolet (UV) index.
+   * @param latitude Latitude
+   * @param longitude Longitude
+   * @param duration Time frame of the returned weather forecast. By default, the forecast data for next hour will be returned. Available values are
+                      1 - Return forecast data for the next hour. Default value.
+                      12 - Return hourly forecast for next 12 hours.
+                      24 - Return hourly forecast for next 24 hours.
+                      72 - Return hourly forecast for next 72 hours (3 days).
+                      120 - Return hourly forecast for next 120 hours (5 days). Only available in S1 SKU.
+                      240 - Return hourly forecast for next 240 hours (10 days). Only available in S1 SKU.
+   * @param language Language in which search results should be returned. Should be one of supported IETF language tags, case insensitive. When data in specified language is not available for a specific field, default language is used. Default value is en-us.
+   * @param unit Specifies to return the data in either metric units or imperial units. Default value is metric.
+   */
+  public getHourlyForecast(latitude: number,
+    longitude: number,
+    duration?: 1 | 12 | 24 | 72 | 120 | 240,
+    language?: string,
+    unit?: Unit
+  ): Observable<HourlyForecastResponse> {
+    let url = `${this._rootUrl}/weather/forecast/hourly/json?api-version=${this._apiVersion}&query=${latitude},${longitude}`;
+
+    if (!(duration === null || duration === undefined)) {
+      url += `&duration=${duration}`;
+    }
+
+    if (!(language === null || language === undefined)) {
+      url += `&language=${language}`;
+    }
+
+    if (!(unit === null || unit === undefined)) {
+      url += `&unit=${unit};`
+    }
+
+    return this.httpClient.get<HourlyForecastResponse>(url);
+  }
+
+  /**
+   * Get Minute Forecast service returns minute-by-minute forecasts for a given location for the next 120 minutes. Users can request weather forecasts in the interval of 1, 5 and 15 minutes. The response will include details such as the type of precipitation (including rain, snow, or a mixture of both), start time, and precipitation intensity value (dBZ).
+   * @param latitude Latitude
+   * @param longitude Longitude
+   * @param interval Specifies time interval in minutes for the returned weather forecast. Supported values are
+                    1 - Retrieve forecast for 1-minute intervals. Returned by default.
+                    5 - Retrieve forecasts for 5-minute intervals.
+                    15 - Retrieve forecasts for 15-minute intervals.
+   * @param language Language in which search results should be returned. Should be one of supported IETF language tags, case insensitive. When data in specified language is not available for a specific field, default language is used. Default value is en-us.
+   */
+  public getMinuteForecast(latitude: number,
+    longitude: number,
+    interval?: 1 | 5 | 15,
+    language?: string
+  ): Observable<MinuteForecastResponse> {
+    let url = `${this._rootUrl}/weather/forecast/minute/json?api-version=${this._apiVersion}&query=${latitude},${longitude}`;
+
+    if (!(interval === null || interval === undefined)) {
+      url += `&interval=${interval}`;
+    }
+
+    if (!(language === null || language === undefined)) {
+      url += `&language=${language}`;
+    }
+
+    return this.httpClient.get<MinuteForecastResponse>(url);
   }
 
 }
